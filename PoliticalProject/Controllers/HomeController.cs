bef5981d4f8d568f60a42340275147ea5cb9f036
thelavax <lory.lavaxservice@gmail.com>
2020-04-26 10:20:40 +0200

    Aggiungere i file di progetto.

---
+++ PoliticalProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PoliticalProject.Models;
+
+namespace PoliticalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Partiti()
+        {
+            return View();
+        }
+
+        public IActionResult ForzaItalia()
+        {
+            return View();
+        }
+
+        public IActionResult ItaliaViva()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
