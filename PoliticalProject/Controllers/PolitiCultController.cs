5fdf8f098a21560455c0ba56ad7696153a55a68d
thelavax <lory.lavaxservice@gmail.com>
2021-05-14 16:52:11 +0200

    Aggiunto articolo- il conflitto bianco nero

--- PoliticalProject/Controllers/PolitiCultController.cs
+++ PoliticalProject/Controllers/PolitiCultController.cs
@@ -119,5 +119,10 @@ namespace PoliticalProject.Controllers
         {
             return View();
         }
+
+        public IActionResult IlConflittoBiancoNero()
+        {
+            return View();
+        }
     }
 }
